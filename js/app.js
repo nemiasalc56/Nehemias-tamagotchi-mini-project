4cc6dd2ea16444218dcc054c34ec16b03e4a30d0
Nehemias Alcantara <nehemiasalcantara@Nehemiass-MacBook-Air.local>
2019-12-19 15:16:25 -0600

    user can enter a name and timer starts running

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
 console.log('Tamagotchi');
 
 class Tamagotchi {
-	constructor(name, age, hunger, boredom, sleepiness, hunger) {
+	constructor(name, age, hunger, boredom, sleepiness) {
 		this.name = name
 		this.age = age
 		this.boredom = boredom
 		this.sleepiness = sleepiness
 		this.hunger = hunger
+
+		console.log(this);
+	}
+	tamagotchi() {
+
 	}
 }
 
 // Object
+const game = {
+	name: '',
+	age: 0,
+	hunger: 0,
+	sleepiness: 0,
+	boredom: 0,
+	timer: 0,
+	setInterval: 0,
+	startGame() {
+		$('.form').text(`Name: ${this.name}`)
+		this.setInterval = setInterval(() => {
+			this.age++
+			console.log('Age ', this.age);
+			this.birthTamagotchi()
+		}, 1800)
 
-
+	},
+	birthTamagotchi() {
+		const newTamagotchi = new Tamagotchi(this.name, this.age, this.hunger, this.boredom, this.sleepiness)
+	}
+}
 
 // Listeners
+// Get name
+$('.form').on('submit', (e) => {
+	e.preventDefault()
+	const $name = $('.name-field')
+	game.name = $name.val()
+	game.startGame()
+
+	$('.name-field').val('')
+})
