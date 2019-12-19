9d01c917507e0cd131ff7452195066a7c0927ff6
Nehemias Alcantara <nehemiasalcantara@Nehemiass-MacBook-Air.local>
2019-12-19 14:16:09 -0600

    added class with properties

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log('Tamagotchi');
+
+class Tamagotchi {
+	constructor(name, age, hunger, boredom, sleepiness, hunger) {
+		this.name = name
+		this.age = age
+		this.boredom = boredom
+		this.sleepiness = sleepiness
+		this.hunger = hunger
+	}
+}
+
+// Object
+
+
+
+// Listeners
